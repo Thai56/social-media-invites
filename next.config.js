bae2c79dd3b42889ebf9fa37bd38a99928a1ccbd
Thai Tran <Thai.Tran5656@gmail.com>
2018-06-16 02:23:39 -0600

    have typescript working in FE

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// // next.config.js
+// const withTypescript = require('@zeit/next-typescript')
+// module.exports = withTypescript()
+//
+// next.config.js
+const withTypescript = require("@zeit/next-typescript")
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = withTypescript({
+  webpack(config, options) {
+    // Do not run type checking twice:
+    if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
+
+    return config
+  }
+})
--- pages/BusinessInfo.js
+++ pages/BusinessInfo.js
@@ -1,30 +1,70 @@
 import React, {Component} from 'react';
-import Button from '@material-ui/core/Button';
+import { Button, TextField } from '@material-ui/core/';
+import axios from 'axios';
 
-/**
- * The input is used to create the `dataSource`, so the input always matches three entries.
- */
+const styles = {
+  formStyle: {
+    width: '500px',
+    height: '200px',
+    margin: 'auto',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  }
+};
+
+const defaultState = {
+  name: '',
+  address: '',
+};
+
 export default class AutoCompleteExampleSimple extends Component {
-  state = {
-    dataSource: [],
-  };
+  state = defaultState;
 
-  handleUpdateInput = (value) => {
-    this.setState({
-      dataSource: [
-        value,
-        value + value,
-        value + value + value,
-      ],
+  handleChange = (e : object) => {
+    const { target: { value, name } } = e;
+    this.setState(() => ({
+      [name]: value,
+    }))
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const URL = 'http://localhost:8080/businessInfo';
+    const { name, address } = this.state;
+    const data = { name, address }; //JSON.stringify();
+    axios.post(URL, data, { 'Content-Type': 'text/html' }).then(res => { 
+      console.log('res.data', res.data);
     });
-  };
+    this.setState(() => (defaultState));
+  }
 
   render() {
     return (
       <div>
-        <Button variant="raised" color="primary">
-          Hello World
-        </Button>
+        <h1 style={{ textAlign: 'center' }}>Please Enter info Below</h1>
+        <form
+          onSubmit={this.handleSubmit}
+          style={styles.formStyle}
+        >
+          <TextField
+            label="Business Title"
+            name="name"
+            margin="normal"
+            value={this.state.name}
+            style={{ width: 400 }}
+            onChange={this.handleChange}
+          />
+          <TextField
+            label="Address"
+            name="address"
+            margin="normal"
+            value={this.state.address}
+            style={{ width: 400 }}
+            onChange={this.handleChange}
+          />
+          <input type="submit" value="submit" />
+        </form>
       </div>
     );
   }
